731dc566df6da252de0320eaa0325633334ac8e7
zhel98 <eldik9861@mail.ru>
2026-09-11 17:05:38 +0600

    херня лютая

--- tests/test1.py
+++ tests/test1.py
@@ -23,23 +23,36 @@ class WarriorHero(MageHero):
         print(f'{self.name} рубит мечом! Уровень {self.lvl}')
         
 class BankAccount():
-  
-    def __init__(self, hero, _balance, __password,bank_name  ):
+    bank_name = 'Optima'
+    def __init__(self, hero, _balance, __password ):
         self.hero = hero
         self._balance = _balance
         self.__password = __password
-        self.bank_name = bank_name
+  
     
     def login(password):
-        if password == password:
-            return True
+        if password == password: return True
         else: return False
     
+    @staticmethod
     def full_info(self):
         print(f'Герой: {self.hero}, Баланс героя: {self.balance} ')
 
-    def get_bank_name(self):
-        print(f'{self.bank_name}')
+    @classmethod
+    def get_bank_name(cls):
+        return cls.bank_name
     
     # def bonus_for_level(self):
     #     return self.bonus_for_level
+    
+    def __str__(self):
+        return f'{self.get_bank_name} | {self._balance} SOM'
+    
+    def __add__(self, other):
+        if type(self) is type(other):
+            return self.__class__(self._balance + other._balance)
+        else:
+            print('Нельзя прибавлять баланс разных персонажей')
+    
+    def __eq__(self, other):
+        if self.full_info.
